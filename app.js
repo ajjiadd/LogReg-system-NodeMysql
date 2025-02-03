09bf420d178938d70b5c2cde6fb3e8ae25e47ef4
ajjiad <ajjiad.cse@gmail.com>
2025-02-03 14:29:52 +0600

    setup nodemon & add function on app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Home Page</h1>");
+});
+
+
+app.listen(3500, ()=>{
+    console.log("Server started on port 3500")
+});
