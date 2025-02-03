326aea923d19222749b1c8de78a14b9e16d0d9e8
ajjiad <ajjiad.cse@gmail.com>
2025-02-03 14:53:26 +0600

    create .env, database on xammp server, and connection for write code

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({path: './.env'})
+
 const app = express();
+
+// connection with database 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "logreg"
+});
+
+//database check error and connection 
+db.connect((error)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("connected to database");
+    }
+})
 
 
 app.get("/", (req, res)=>{
